32486b548189f57f9d9e4c81ef61c142f34036aa
James <Icedshot@gmail.com>
2020-06-26 18:20:49 +0100

    hello world

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <imgui/imgui.h>
+#include <imgui/imgui_internal.h>
+#include <imgui/examples/imgui_impl_glfw.h>
+#include <imgui/examples/imgui_impl_opengl3.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+//#include "window_context.hpp"
+#include <toolkit/render_window.hpp>
+#include <vec/vec.hpp>
+
+int main()
+{
+    return 0;
+}
