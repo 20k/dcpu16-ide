7b8bb7923b79c79708d24bad1a95650678a3e420
James <Icedshot@gmail.com>
2020-06-26 22:52:48 +0100

    migrate into base_ide

---
+++ base_ide.hpp
@@ -0,0 +1,99 @@
+#ifndef BASE_IDE_HPP_INCLUDED
+#define BASE_IDE_HPP_INCLUDED
+
+#include <imguicolortextedit/TextEditor.h>
+
+namespace dcpu
+{
+    inline
+    const std::map<std::string, std::string>& instruction_to_description()
+    {
+        static std::map<std::string, std::string> instr
+        {
+             {"SET", "SET b, a : Sets b to a"},
+             {"ADD", "ADD b, a : Sets b to b+a, sets EX to 0x0001 on overflow"},
+             {"SUB","SUB b, a : Sets b to b-a, sets EX to 0xffff on underflow"},
+             {"MUL","MUL b, a : Sets b to b * a, sets EX to ((b*a)>>16)&0xffff. (b, a) are unsigned"},
+             {"MLI","MLI b, a : Like MUL. (b, a) are signed"},
+             {"DIV","DIV b, a : Sets b to b/a, sets EX to ((b<<16)/a)&0xffff. If a == 0, sets b and EX to 0. (b, a) are unsigned"},
+             {"DVI","DVI b, a : Like DIV. (b, a) are unsigned. Rounds towards 0"},
+             {"MOD","MOD b, a : Sets b to b%a. If a==0, sets b to 0 instead"},
+             {"MDI","MDI b, a : Like MOD. (b, a) are signed"},
+             {"AND","AND b, a : Sets b to b&a"},
+             {"BOR","BOR b, a : Sets b to b|a"},
+             {"XOR","XOR b, a : Sets b to b^a"},
+             {"SHR","SHR b, a : Sets b to b>>>a, sets EX to ((b<<16)>>a)&0xffff. Logical shift"},
+             {"ASR","ASR b, a : Sets b to b>>a, sets EX to ((b<<16)>>>a)&0xffff. Arithmetic shift. (b) is signed"},
+             {"SHL","SHL b, a : Sets b to b<<a, sets EX to ((b<<a)>>16)&0xffff"},
+             {"IFB","IFB b, a : Performs next instruction only if (b&a)!=0"},
+             {"IFC","IFC b, a : Performs next instruction only if (b&a)==0"},
+             {"IFE","IFE b, a : Performs next instruction only if b==a"},
+             {"IFN","IFN b, a : Performs next instruction only if b!=a"},
+             {"IFG","IFG b, a : Performs next instruction only if b>a"},
+             {"IFA","IFA b, a : Performs next instruction only if b>a (signed)"},
+             {"IFL","IFL b, a : Performs next instruction only if b<a"},
+             {"IFU","IFU b, a : Performs next instruction only if b<a (signed)"},
+             {"ADX","ADX b, a : Sets b to b+a+EX, sets EX to 0x0001 on overflow, otherwise 0x0"},
+             {"SBX","SBX b, a : Sets b to b-a+EX, sets EX to 0xffff on overflow, otherwise 0x0"},
+             {"SND","SND b, a : Sends the value b to the hardware with identifier a, blocks until sent"},
+             {"RCV","RCV b, a : Sets b to a value received from the hardware with identifier a, blocks until received"},
+             {"STI","STI b, a : Sets b to a, then increases I and J by 1"},
+             {"STD","STD b, a : Sets b to a, then decreases I and J by 1"},
+             {"JSR","JSR a : Pushes the address of the next instruction to the stack, then sets PC to a"},
+             {"INT","INT a : Triggers a software interrupt with message a"},
+             {"IAG","IAG a : Sets a to IA"},
+             {"IAS","IAS a : Sets IA to a"},
+             {"RFI","TFI a : Disables interrupt queueing, pops A from the stack, then pops PC from the stack"},
+             {"IAQ","IAQ a : If a is nonzero, interrupts will be added to the queue instead of triggered. If a is 0, interrupts will be triggered as normal again"},
+             {"HWN","HWN a : Sets a to the number of connected hardware devices"},
+             {"HWQ","HSQ a : Sets A, B, C, X, Y registers to information about hardware a. A+(B<<16) is a 32 bit word identifying the hardware id. C is hardware version. X+(Y<<16) is a 32bit word identifying the manufacturer"},
+             {"HWI","HWI a : Sends an interrupt to hardware a"},
+             {"IFW","IFW a : Performs next instruction only if the hardware identified by a is waiting to write a value"},
+             {"IFR","IFR a : Performs next instruction only if the hardware identified by a is waiting to read a value"},
+             {"BRK","BRK : Pauses execution"},
+        };
+
+        return instr;
+    }
+
+    inline
+    std::string get_description_of_instruction(const std::string& in)
+    {
+        const std::map<std::string, std::string>& mapping = instruction_to_description();
+
+        if(auto it = mapping.find(in); it != mapping.end())
+            return it->second;
+
+        return "No such instruction";
+    }
+
+    namespace ide
+    {
+        struct editor
+        {
+            TextEditor edit;
+
+            editor()
+            {
+                auto lang = TextEditor::LanguageDefinition::CPlusPlus();
+                auto mapping = dcpu::instruction_to_description();
+
+                for(auto& i : mapping)
+                {
+                    TextEditor::Identifier id;
+                    id.mDeclaration = i.second;
+                    lang.mIdentifiers.insert(std::make_pair(i.first, id));
+                }
+
+                edit.SetLanguageDefinition(lang);
+            }
+
+            void render()
+            {
+                edit.Render("IDE");
+            }
+        };
+    }
+}
+
+#endif // BASE_IDE_HPP_INCLUDED
--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <toolkit/render_window.hpp>
 #include <vec/vec.hpp>
 #include <imguicolortextedit/TextEditor.h>
+#include "base_ide.hpp"
 
 int main()
 {
@@ -17,111 +18,7 @@ int main()
 
     render_window win(sett, "DCPU16-IDE");
 
-    TextEditor edit;
-
-    auto lang = TextEditor::LanguageDefinition::CPlusPlus();
-
-    static const char* identifiers[] = {"SET",
-                                         "ADD",
-                                         "SUB",
-                                         "MUL",
-                                         "MLI",
-                                         "DIV",
-                                         "DVI",
-                                         "MOD",
-                                         "MDI",
-                                         "AND",
-                                         "BOR",
-                                         "XOR",
-                                         "XHR",
-                                         "ASR",
-                                         "SHL",
-                                         "IFB",
-                                         "IFC",
-                                         "IFE",
-                                         "IFN",
-                                         "IFG",
-                                         "IFA",
-                                         "IFL",
-                                         "IFU",
-                                         "ADX",
-                                         "SBX",
-                                         "SND",
-                                         "RCV",
-                                         "STI",
-                                         "STD",
-                                         "JSR",
-                                         "INT",
-                                         "IAG",
-                                         "IAS",
-                                         "RFI",
-                                         "IAQ",
-                                         "HWN",
-                                         "HWQ",
-                                         "HWI",
-                                         "IFW",
-                                         "IFR",
-                                         "BRK"};
-
-    static const char* descriptions[] = {
-        "SET b, a : Sets b to a",
-        "ADD b, a : Sets b to b+a, sets EX to 0x0001 on overflow",
-        "SUB b, a : Sets b to b-a, sets EX to 0xffff on underflow",
-        "MUL b, a : Sets b to b * a, sets EX to ((b*a)>>16)&0xffff. (b, a) are unsigned",
-        "MLI b, a : Like MUL. (b, a) are signed",
-        "DIV b, a : Sets b to b/a, sets EX to ((b<<16)/a)&0xffff. If a == 0, sets b and EX to 0. (b, a) are unsigned",
-        "DVI b, a : Like DIV. (b, a) are unsigned. Rounds towards 0",
-        "MOD b, a : Sets b to b%a. If a==0, sets b to 0 instead",
-        "MDI b, a : Like MOD. (b, a) are signed",
-        "AND b, a : Sets b to b&a",
-        "BOR b, a : Sets b to b|a",
-        "XOR b, a : Sets b to b^a",
-        "SHR b, a : Sets b to b>>>a, sets EX to ((b<<16)>>a)&0xffff. Logical shift",
-        "ASR b, a : Sets b to b>>a, sets EX to ((b<<16)>>>a)&0xffff. Arithmetic shift. (b) is signed",
-        "SHL b, a : Sets b to b<<a, sets EX to ((b<<a)>>16)&0xffff",
-        "IFB b, a : Performs next instruction only if (b&a)!=0",
-        "IFC b, a : Performs next instruction only if (b&a)==0",
-        "IFE b, a : Performs next instruction only if b==a",
-        "IFN b, a : Performs next instruction only if b!=a",
-        "IFG b, a : Performs next instruction only if b>a",
-        "IFA b, a : Performs next instruction only if b>a (signed)",
-        "IFL b, a : Performs next instruction only if b<a",
-        "IFU b, a : Performs next instruction only if b<a (signed)",
-        "ADX b, a : Sets b to b+a+EX, sets EX to 0x0001 on overflow, otherwise 0x0",
-        "SBX b, a : Sets b to b-a+EX, sets EX to 0xffff on overflow, otherwise 0x0",
-        "SND b, a : Sends the value b to the hardware with identifier a, blocks until sent",
-        "RCV b, a : Sets b to a value received from the hardware with identifier a, blocks until received",
-        "STI b, a : Sets b to a, then increases I and J by 1",
-        "STD b, a : Sets b to a, then decreases I and J by 1",
-        "JSR a : Pushes the address of the next instruction to the stack, then sets PC to a",
-        "INT a : Triggers a software interrupt with message a",
-        "IAG a : Sets a to IA",
-        "IAS a : Sets IA to a",
-        "TFI a : Disables interrupt queueing, pops A from the stack, then pops PC from the stack",
-        "IAQ a : If a is nonzero, interrupts will be added to the queue instead of triggered. If a is 0, interrupts will be triggered as normal again",
-        "HWN a : Sets a to the number of connected hardware devices",
-        "HSQ a : Sets A, B, C, X, Y registers to information about hardware a. A+(B<<16) is a 32 bit word identifying the hardware id. C is hardware version. X+(Y<<16) is a 32bit word identifying the manufacturer",
-        "HWI a : Sends an interrupt to hardware a",
-        "IFW a : Performs next instruction only if the hardware identified by a is waiting to write a value",
-        "IFR a : Performs next instruction only if the hardware identified by a is waiting to read a value",
-        "BRK : Pauses execution",
-    };
-
-    /*for (int i = 0; i < sizeof(identifiers) / sizeof(identifiers[0]); ++i)
-	{
-		TextEditor::Identifier id;
-		id.mDeclaration = descriptions[i];
-		lang.mPreprocIdentifiers.insert(std::make_pair(std::string(identifiers[i]), id));
-	}*/
-
-	for (int i = 0; i < sizeof(identifiers) / sizeof(identifiers[0]); ++i)
-	{
-		TextEditor::Identifier id;
-		id.mDeclaration = std::string(descriptions[i]);
-		lang.mIdentifiers.insert(std::make_pair(std::string(identifiers[i]), id));
-	}
-
-	edit.SetLanguageDefinition(lang);
+    dcpu::ide::editor edit;
 
     while(!win.should_close())
     {
@@ -129,7 +26,19 @@ int main()
 
         ImGui::Begin("Hello");
 
-        edit.Render("Hithere");
+        if(ImGui::BeginMenuBar())
+        {
+            if(ImGui::BeginMenu("Instructions"))
+            {
+                ImGui::MenuItem("Test");
+
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMenuBar();
+        }
+
+        edit.render();
 
         ImGui::End();
 
