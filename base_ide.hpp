52274fa40791e52d9dc57280804edf37aebf255a
20k <icedshot@gmail.com>
2020-06-29 14:10:36 +0100

    migrate things around

--- base_ide.hpp
+++ base_ide.hpp
@@ -54,9 +54,30 @@ namespace dcpu
 
     namespace ide
     {
+        inline
+        void register_editor(const std::string& name, uint16_t& val)
+        {
+            int ival = val;
+
+            ImGui::Text(name.c_str());
+
+            ImGui::SameLine();
+
+            ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth());
+
+            //ImGui::PushItemWidth(ImGui::CalcTextSize("65535").x*4 + 40);
+
+            ImGui::InputInt(("##" + name).c_str(), &ival);
+
+            val = ival;
+        }
+
         struct editor
         {
             TextEditor edit;
+            CPU c;
+            stack_vector<uint16_t, MEM_SIZE> translation_map;
+            bool halted = false;
 
             editor()
             {
@@ -75,7 +96,76 @@ namespace dcpu
 
             void render()
             {
+                ImGui::Begin("Hello");
+
+                ImGui::BeginChild("Child", ImVec2(400, 0));
+
+                {
+                    std::unordered_set<int> current_pc;
+
+                    int line = 0;
+                    int seek_character = translation_map[c.regs[PC_REG]];
+
+                    std::string seek = edit.GetText();
+
+                    while(seek_character < seek.size() && should_prune(seek[seek_character]))
+                        seek_character++;
+
+                    for(int i=0; i <= seek_character && i < seek.size(); i++)
+                    {
+                        if(seek[i] == '\n')
+                            line++;
+                    }
+
+                    current_pc.insert(line+1);
+
+                    edit.SetBreakpoints(current_pc);
+                }
+
                 edit.Render("IDE");
+
+                ImGui::EndChild();
+
+                ImGui::SameLine();
+
+                ImGui::BeginGroup();
+
+                if(ImGui::Button("Assemble"))
+                {
+                    auto [rinfo_opt, err] = assemble(edit.GetText());
+
+                    if(rinfo_opt.has_value())
+                    {
+                        c = CPU();
+                        c.load(rinfo_opt.value().mem, 0);
+                        halted = false;
+
+                        translation_map = rinfo_opt.value().translation_map;
+                    }
+                }
+
+                if(ImGui::Button("Step"))
+                {
+                    if(!halted)
+                        halted = halted || c.step();
+                }
+
+                register_editor("A: ", c.regs[A_REG]);
+                register_editor("B: ", c.regs[B_REG]);
+                register_editor("C: ", c.regs[C_REG]);
+                register_editor("X: ", c.regs[X_REG]);
+                register_editor("Y: ", c.regs[Y_REG]);
+                register_editor("Z: ", c.regs[Z_REG]);
+                register_editor("I: ", c.regs[I_REG]);
+                register_editor("J: ", c.regs[J_REG]);
+                register_editor("PC:", c.regs[PC_REG]);
+                register_editor("SP:", c.regs[SP_REG]);
+                register_editor("EX:", c.regs[EX_REG]);
+                register_editor("IA:", c.regs[IA_REG]);
+
+                ImGui::EndGroup();
+
+                ImGui::End();
             }
         };
 
--- main.cpp
+++ main.cpp
@@ -50,24 +50,7 @@ IFA X, 0
 SET PC, loop
 SET Z, 1*/
 
-///TODO: Communication channels
-void register_editor(const std::string& name, uint16_t& val)
-{
-    int ival = val;
-
-    ImGui::Text(name.c_str());
-
-    ImGui::SameLine();
-
-    ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth());
-
-    //ImGui::PushItemWidth(ImGui::CalcTextSize("65535").x*4 + 40);
-
-    ImGui::InputInt(("##" + name).c_str(), &ival);
-
-    val = ival;
-}
-
+///TODO: Communication channelss
 int main()
 {
     render_settings sett;
@@ -76,90 +59,17 @@ int main()
 
     render_window win(sett, "DCPU16-IDE");
 
-    bool halted = false;
-    CPU c;
     dcpu::ide::editor edit;
     dcpu::ide::reference_card card;
 
-    stack_vector<uint16_t, MEM_SIZE> translation_map;
+    std::vector<CPU> cpu_count;
 
     while(!win.should_close())
     {
         win.poll();
 
-        ImGui::Begin("Hello");
-
-        ImGui::BeginChild("Child", ImVec2(400, 0));
-
-        {
-            std::unordered_set<int> current_pc;
-
-            int line = 0;
-            int seek_character = translation_map[c.regs[PC_REG]];
-
-            std::string seek = edit.edit.GetText();
-
-            while(seek_character < seek.size() && should_prune(seek[seek_character]))
-                seek_character++;
-
-            for(int i=0; i <= seek_character && i < seek.size(); i++)
-            {
-                if(seek[i] == '\n')
-                    line++;
-            }
-
-            current_pc.insert(line+1);
-
-            edit.edit.SetBreakpoints(current_pc);
-        }
-
-        edit.render();
         card.render();
-
-        ImGui::EndChild();
-
-        ImGui::SameLine();
-
-        ImGui::BeginGroup();
-
-        if(ImGui::Button("Assemble"))
-        {
-            auto [rinfo_opt, err] = assemble(edit.edit.GetText());
-
-            if(rinfo_opt.has_value())
-            {
-                c = CPU();
-                c.load(rinfo_opt.value().mem, 0);
-                halted = false;
-
-                translation_map = rinfo_opt.value().translation_map;
-            }
-        }
-
-        if(ImGui::Button("Step"))
-        {
-            if(!halted)
-                halted = halted || c.step();
-        }
-
-        register_editor("A: ", c.regs[A_REG]);
-        register_editor("B: ", c.regs[B_REG]);
-        register_editor("C: ", c.regs[C_REG]);
-        register_editor("X: ", c.regs[X_REG]);
-        register_editor("Y: ", c.regs[Y_REG]);
-        register_editor("Z: ", c.regs[Z_REG]);
-        register_editor("I: ", c.regs[I_REG]);
-        register_editor("J: ", c.regs[J_REG]);
-        register_editor("PC:", c.regs[PC_REG]);
-        register_editor("SP:", c.regs[SP_REG]);
-        register_editor("EX:", c.regs[EX_REG]);
-        register_editor("IA:", c.regs[IA_REG]);
-
-        ImGui::EndGroup();
-
-        //ImGui::Text("Hello");
-
-        ImGui::End();
+        edit.render();
 
         sf::sleep(sf::milliseconds(1));
 
